5f9e0eff1855c7736340b0392c493dd587b11880
Rômulo Oliveira DEV <romuloffall@gmail.com>
2023-11-28 23:54:52 -0300

    #query optimize

--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -14,15 +14,23 @@
             @endif
         </div>
         <div class="flex items-center space-x-4 font-light ">
+            <x-label>
+                <span class="text-gray-500">Popular</span>
+            </x-label>
+            <x-checkbox wire:model.live="popular" />
             <button class="{{ $sort === 'desc' ? 'text-gray-900 py-4 border-b border-gray-700' : 'text-gray-500 py-4' }}"
-                wire:click="setSort('desc')">Latest</button>
+                x-data="{ sort: '{{ $sort }}' }" x-on:click="sort = 'desc'; $wire.setSort('desc')">
+                Latest
+            </button>
             <button class="{{ $sort === 'asc' ? 'text-gray-900 py-4 border-b border-gray-700' : 'text-gray-500 py-4' }}"
-                wire:click="setSort('asc')">Oldest</button>
+                x-data="{ sort: '{{ $sort }}' }" x-on:click="sort = 'asc'; $wire.setSort('asc')">
+                Oldest
+            </button>
         </div>
     </div>
     <div class="py-4">
         @foreach ($this->posts as $post)
-            <x-posts.post-item :post="$post" />
+            <x-posts.post-item wire:key="{{ $post->id }}" :post="$post" />
         @endforeach
     </div>
 
